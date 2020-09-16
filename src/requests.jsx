8a7002a1fe0b7149f6ccc206897bb39cb2e1e334
kingz10102 <kingz10102@gmail.com>
2020-09-16 19:23:51 -0400

    this a commit

--- src/requests.jsx
+++ src/requests.jsx
@@ -1,5 +1,5 @@
 const API_KEY = '9e48e46941b49cb1566bd41c28833612';
-
+// api
 export default {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,

a8ef5f2299962c55c68e48042a513366680e6251
kingz10102 <kingz10102@gmail.com>
2020-09-16 19:23:19 -0400

    this a commit

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import Heading from './components/Heading';
 import NavBar from './components/NavBar';
 import Results from './components/Results';
 import requests from './requests';
+
+
 import './App.css';
+
 function App() {
   const [chosenOption, setChosenOption] = useState(requests.fetchTrending);
   
   
   return (
     <div className="app">
-     {/* Heading */}
+    {/* Heading */}
      <Heading/>
      {/* Navbar */}
      <NavBar setChosenOption={setChosenOption}/>
